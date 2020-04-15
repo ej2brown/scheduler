6bd3119df6eba15bc51930ad3ae2d69b85e8d365
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-15 15:29:01 -0400

    create status component

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -6,10 +6,10 @@ export default function Confirm(props) {
   const { message, onConfirm, onCancel } = props;
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button onClick={onCancel} danger>Cancel</Button>
+        <Button onClick={onConfirm} danger>Confirm</Button>
       </section>
     </main>
   );
