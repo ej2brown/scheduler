55681a9d871f49045ec21bc29fdf34009d93e7b0
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-17 13:04:29 -0400

    add promise for interviews

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,54 +1,64 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import "components/Application.scss";
-
 import DayList from "components/DayList";
-
 import Appointment from "components/Appointment/index";
-
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
-    day: 'Monday',
+    day: "Monday",
     days: [],
     appointments: {},
     interviewers: {},
   });
 
-  const setDay = (day) => setState({ ...state, day });
-
   /*requests to endpoints for data*/
-  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const fetchData = () => {
     Promise.all([
       Promise.resolve(axios.get("/api/days")),
       Promise.resolve(axios.get("/api/appointments")),
-    ]).then((all) => {
-
-      const [days, appointments] = all;
-
-      setState((oldData) => ({
-        ...oldData,
-        days: [...days.data],
-        appointments: { ...appointments.data },
-      }));
-    })
-    .catch(error => console.log(error))
+      Promise.resolve(axios.get("/api/interviewers")),
+    ])
+      .then((all) => {
+        const [days, appointments, interviewers] = all;
+        console.log("all", all);
+        setState((oldData) => ({
+          ...oldData,
+          days: [...days.data],
+          appointments: { ...appointments.data },
+          interviewers: { ...interviewers.data },
+        }));
+      })
+      .catch((error) => console.log(error));
   };
-
-  useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      console.log(response);
-      fetchData();
-    });
-  }, []);
 
   const { day, days } = state;
 
+  /* transform to reduce interviw data duplication */
   const appointments = getAppointmentsForDay(state, state.day);
-  console.log('appointments',appointments); //TODO fix loop 
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+  
+  /* console.logs to delete */
+  console.log("schedule", schedule);
+  console.log("appointments", appointments);
+  console.log("day", day);
+  console.log("days", days);
+  console.log("state.interviewers", state.interviewers);
 
   return (
     <main className="layout">
@@ -60,7 +70,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={days} day={day} setDay={day => setState(prev =>({ ...state, day }))}/>
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
