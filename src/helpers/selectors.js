d0ea281e323dc83807474bda30883c69c126ac2a
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-17 13:03:27 -0400

    add function to return an interview object

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,14 @@
+/* returns array of appointments matched for that day */
 export function getAppointmentsForDay(state, day) {
   const currentDay = state.days.filter((obj) => obj.name === day);
   if (currentDay.length === 0) return [];
   return currentDay[0].appointments.map((id) => state.appointments[id]);
 };
+
+/* returns interview object */
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  const { student, interviewer } = interview;
+  return { student, interviewer: state.interviewers[interviewer] };
+};
+
