0c9cfaf0019bdfd8c1f10d2ff402d14adc71f484
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-18 13:08:20 -0400

    fix bug by changing variable name from list to interviewers

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,27 +1,27 @@
 import React from "react";
 
 import "components/InterviewerListItem.scss";
+import InterviewerListItem from "components/InterviewerListItem.js";
 
-export default function InterviewerListItem(props) {
-    const { id, name, avatar, selected, onChange, interviewers} = props;
+export default function InterviewerList(props) {
+  const { id, name, avatar, selected, onChange, interviewers } = props;
 
-
-    const InterviewerList = interviewers.map((interviewer) => {
-        return <InterviewerListItem
+  const Interviewers = interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
-        avatar= {interviewer.avatar}
+        avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
         setInterview={(event) => onChange(interviewer.id)}
-         />;
-        });
+      />
+    );
+  });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-        {InterviewerList}
-      </ul>
+      <ul className="interviewers__list">{Interviewers}</ul>
     </section>
   );
 }
