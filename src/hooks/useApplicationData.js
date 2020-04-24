9097feb7147a177e3b9eb4c30d711ca2d559152b
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-23 22:14:22 -0400

    clean up comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -57,6 +57,7 @@ export default function useApplicationData() {
             }
         };
         fetchData();
+        return () => webSocket.close();
     }, []);
 
     const fetchData = () => {
